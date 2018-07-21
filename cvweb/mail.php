cf61600e66a3cababde0db442a3e12ccd3e6193b
chioranionutcatalin <chioranionutcatalin@gmail.com>
2018-07-21 18:31:19 +0300

    asd
    asdas

--- cvweb/mail.php
+++ cvweb/mail.php
@@ -1,11 +1,12 @@
 <?php 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "chioranionutcatalin@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+    $name = $_POST['name'];
+    die($name);
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $formcontent="From: $name \n Message: $message";
+    $recipient = "chioranionutcatalin@gmail.com";
+    $subject = "Contact Form";
+    $mailheader = "From: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    echo "Thank You!";
 ?>
